efb48beb425b7448d259347bdb2f6da5086604af
Jennifer Yu <jennyyu163@yahoo.ca>
2021-06-21 17:47:00 -0400

    create a bionic testing file

---
+++ bionic/run.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import time
+from pathlib import Path
+from .train import Trainer
+from .utils.common import create_time_taken_string
+import sys
+from pathlib import Path
+
+
+
+
+# TODO: add config params to cli
+def train(config_path: Path):
+    """Integrates networks using BIONIC.
+
+    All relevant parameters for the model should be specified in a `.json` config file.
+
+    See https://github.com/bowang-lab/BIONIC/blob/master/README.md for details on writing
+    the config file, as well as usage tips.
+    """
+    time_start = time.time()
+    trainer = Trainer(config_path)
+    trainer.train()
+    trainer.test()
+    trainer.forward()
+    time_end = time.time()
+    print(create_time_taken_string(time_start, time_end))
+
+
+def main():
+    train(Path('/Users/jyu/Documents/BIONIC/bionic/config/patient_similarity.json'))
+
+if __name__ == '__main__':
+    main()
+
